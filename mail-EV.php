cc0c788d06cebb00e7c43a453b99e83a71a07329
thearcoftheus <thearcoftheus@gmail.com>
2016-05-16 11:35:14 -0400

    Create mail-EV.php

---
+++ mail-EV.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<h1> The Arc Voting App Form Proof of Concept </h1>
+<form action="post.php" method="post">
+First Name: <input type="text" name="name"><br>
+Last Name: <input type="text" name="l_name"><br>
+Address: <input type="text" name="address"><br>
+City: <input type="text" name="city"><br>
+Cell Phone: <input type="text" name="phone"><br>
+EIN: <input type="text" name="ein"><br>
+E-mail: <input type="text" name="email"><br>
+Subject: <input type="text" name="subject"><br>
+Body:<br> <textarea name="body" rows="4" cols="50"></textarea><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+Status API Training Shop Blog About
